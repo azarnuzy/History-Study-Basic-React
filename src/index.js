be70b4f88ba8f636218804f0c9fc8848ba10187e
azarnuzy <azarnuzy@gmail.com>
2021-10-17 14:39:15 +0700

    learning about state and props

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import StateProps from './stateProps';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+    <StateProps />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
