5d539fd53a8fdd8f10e31a9ccdd9b2f9d1d3db3f
azarnuzy <azarnuzy@gmail.com>
2021-10-19 09:46:37 +0700

    showing the product but still can't show the img on the product

--- src/ReactAPI/index.js
+++ src/ReactAPI/index.js
@@ -1,12 +1,37 @@
 import React, { Component } from 'react';
 import NavbarComponent from './NavbarComponent';
 import ListCategories from './ListCategories';
-import './CSS/style.css';
 import { Col, Row, Container } from 'react-bootstrap';
 import Hasil from './Hasil';
+import Menus from './Menus';
+import { API_URL } from './utils/constant';
+import axios from 'axios';
+import './CSS/style.css';
 
 export class ReactAPI extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      menus: [],
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get(API_URL + '/products')
+      .then((res) => {
+        const menus = res.data;
+        this.setState({ menus });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
   render() {
+    const { menus } = this.state;
+    console.log(this.state.menus);
     return (
       <div className="App">
         <NavbarComponent />
@@ -19,6 +44,10 @@ export class ReactAPI extends Component {
                   <strong>Daftar Produk</strong>
                   <hr />
                 </h4>
+                <Row>
+                  {menus &&
+                    menus.map((menu) => <Menus key={menu.id} menu={menu} />)}
+                </Row>
               </Col>
               <Hasil />
             </Row>
